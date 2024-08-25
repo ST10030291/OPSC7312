303bbefc00e61fd61d7839f8ac2ca7f83fe26777
ST10030291 <ST10030291@vcconnect.edu.za>
2024-08-25 15:03:30 +0200

    Added SSO feature/ Google SSO

--- OPSC7312_BudgetBuddy/app/build.gradle.kts
+++ OPSC7312_BudgetBuddy/app/build.gradle.kts
@@ -48,6 +48,10 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.database)
     implementation(libs.firebase.auth.ktx)
+    implementation(platform("com.google.firebase:firebase-bom:31.5.0"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.android.gms:play-services-auth:20.5.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

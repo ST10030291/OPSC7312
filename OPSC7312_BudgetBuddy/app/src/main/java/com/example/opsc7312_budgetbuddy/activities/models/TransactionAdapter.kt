92a5ade7ccfa1a1dcbf6c746e728317ba3eb5597
Ditesh <dajeekaushil@gmail.com>
2024-09-26 07:58:47 +0200

    Updated the transaction Item

--- OPSC7312_BudgetBuddy/app/src/main/java/com/example/opsc7312_budgetbuddy/activities/models/TransactionAdapter.kt
+++ OPSC7312_BudgetBuddy/app/src/main/java/com/example/opsc7312_budgetbuddy/activities/models/TransactionAdapter.kt
@@ -11,21 +11,21 @@ class TransactionAdapter(private val transactions: List<TransactionItem>) :
     RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
 
     class TransactionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val transactionName: TextView = itemView.findViewById(R.id.transaction_name)
-        val transactionAmount: TextView = itemView.findViewById(R.id.transaction_amount)
-        val transactionCategory: TextView = itemView.findViewById(R.id.transaction_category)
+        val transactionName: TextView = itemView.findViewById(R.id.categoryExpense)
+        val transactionAmount: TextView = itemView.findViewById(R.id.displayCost)
+        val transactionCategory: TextView = itemView.findViewById(R.id.displayCategory)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_transaction, parent, false)
+            .inflate(R.layout.transaction_item, parent, false)
         return TransactionViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
         val transaction = transactions[position]
         holder.transactionName.text = transaction.name
-        holder.transactionAmount.text = transaction.amount
+        holder.transactionAmount.text = "R" + transaction.amount
         holder.transactionCategory.text = transaction.category
     }
 
